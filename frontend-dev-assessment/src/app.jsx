249a6131909738646875947f4ef7949f55007b93
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-06-19 09:58:45 -0700

    refactored to meet style guide

--- frontend-dev-assessment/src/app.jsx
+++ frontend-dev-assessment/src/app.jsx
@@ -1,25 +1,24 @@
-//React
+// React;
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Component } from 'react';
-//Files
-import BoardContainer from './containers/BoardContainer.jsx'
+// Files;
+import BoardContainer from './containers/BoardContainer.jsx';
 
 
 export default class App extends Component {
     render() {
-        const numberOfBoardContainers = new Array(1, 2, 3)
+        const numberOfBoardContainers = [1, 2, 3];
         const threeBoards = numberOfBoardContainers.map((ele, ind) => {
-            return <div className='boards-container' key={ind}>
-                <BoardContainer id={ind} key={ind + 1} />
+            return <div className="boards-container" key={'wrapBoardContainer' + ind}>
+                <BoardContainer id={ind}
+                    key={'BoardContainer' + ind} />
             </div>
-        })
+        });
         return (
             <div>
                 {threeBoards}
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
-// ReactDOM.render(<App />, document.getElementById('app'))
